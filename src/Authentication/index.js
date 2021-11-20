047555279e841c7c965452faa15470d60d67b404
Abhishek Kumar <abhishekk@NSL-4851.local>
2021-11-20 22:58:25 +0530

    refresh token addded

--- src/Authentication/index.js
+++ src/Authentication/index.js
@@ -53,7 +53,7 @@ export default (app) => {
                                         expiresIn: "2h",
                                     }
                                 );
-                                res.status(201).json(token);
+                                res.status(201).json({token :token});
 
                             }
 
@@ -98,7 +98,7 @@ export default (app) => {
                                 }
                             );
 
-                            res.status(200).json(token);
+                            res.status(200).json({token :token});
                         }
                         else{
                             res.status(400).send("Invalid Credentials");
@@ -119,6 +119,45 @@ export default (app) => {
             console.log(err);
         }
     });
+
+    app.post("/refresh", verifyToken , (req,res) => {
+        const token =
+        req.body.token || req.query.token || req.headers["x-access-token"];
+
+        if (!token) {
+            return res.status(401).end()
+        }
+    
+        var payload
+        try {
+            payload = jwt.verify(token, TOKEN_KEY)
+        } catch (e) {
+            if (e instanceof jwt.JsonWebTokenError) {
+                return res.status(401).end()
+            }
+            return res.status(400).end()
+        }
+        // (END) The code uptil this point is the same as the first part of the `welcome` route
+    
+        // We ensure that a new token is not issued until enough time has elapsed
+        // In this case, a new token will only be issued if the old token is within
+        // 30 seconds of expiry. Otherwise, return a bad request status
+        // const nowUnixSeconds = Math.round(Number(new Date()) / 1000)
+        // if (payload.exp - nowUnixSeconds > 30) {
+        //     return res.status(400).end()
+        // }
+    
+        // Now, create a new token for the current user, with a renewed expiration time
+        const newToken = jwt.sign({ user_id: payload.user_id , email : payload.email }, TOKEN_KEY, {
+           
+            expiresIn: "2h",
+        })
+    
+        // Set the new token as the users `token` cookie
+        // res.cookie("token", newToken, { maxAge: jwtExpirySeconds * 1000 })
+        // res.end()
+        res.status(200).json({token : newToken});
+    })
 
     app.get("/welcome", verifyToken, (req, res) => {
         res.status(200).send("Welcome 🙌 ");
